d223d5b4ee8accf844fc5b53a6d4cc9cf56f1444
succhi111 <suchendramishra396@gmail.com>
2022-09-05 20:55:03 +0530

    added:Program to check Automorphic number

---
+++ src/Automorphic.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Automorphic {
+    public static void main(String[] args) {
+
+        int p = 0, d = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check");
+
+        int n = sc.nextInt();
+        int sq = n * n;
+
+        while (p < n) {
+            p = sq % d;
+            d = d * 10;
+        }
+
+        if (p == n)
+            System.out.println(n + " is automorphic");
+        else
+            System.out.println(n + " is not automorphic");
+    }
+}
+
