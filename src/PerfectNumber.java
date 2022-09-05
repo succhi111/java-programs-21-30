c2c47cdffbdc229a1179f051954bfe48a912f02b
succhi111 <suchendramishra396@gmail.com>
2022-09-05 17:55:02 +0530

    added:Program to check perfect number

---
+++ src/PerfectNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int n, sum = 0;
+        System.out.println("Enter any number");
+
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+
+            }
+        }
+        if (n == sum)
+            System.out.println("PERFECT NUMBER");
+        else
+            System.out.println("NOT A PERFECT NUMBER");
+
+    }
+}
