e439966e78ebc8335236c0325a52af0e40bffa8d
succhi111 <suchendramishra396@gmail.com>
2022-09-05 18:08:12 +0530

    added:Program to check perfect square

---
+++ src/PerfectSquare.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number you want to check");
+        int num = sc.nextInt();
+
+        if (Math.ceil(Math.sqrt(num)) == Math.floor(Math.sqrt(num)))
+        {
+            System.out.print(num + " is a Perfect Square");
+        }
+        else
+        {
+            System.out.print(num + " is not a Perfect Square");
+        }
+    }
+
+}
