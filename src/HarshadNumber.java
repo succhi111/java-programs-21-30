f47667835abd4bbac641012f9d8ab83718d0caa8
succhi111 <suchendramishra396@gmail.com>
2022-09-06 11:27:46 +0530

    added:Program to check Harshad number

--- src/HarshadNumber.java
+++ src/HarshadNumber.java
@@ -1,24 +1,25 @@
 //sum of digits of the number is completely devide by number
 //eg:- 126=1+2+6=9 and 126%9=0
+
 import java.util.Scanner;
 
 public class HarshadNumber {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check");
-        int n= sc.nextInt();
+        int n = sc.nextInt();
 
-        int num=n;
-        int sum=0;
-        while(num!=0){
-            int rem=num%10;
-            sum=sum+rem;
-            num=num/10;
+        int num = n;
+        int sum = 0;
+        while (num != 0) {
+            int rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
         }
-        if (n%sum==0)
-            System.out.println(n+ " is Harshad number");
+        if (n % sum == 0)
+            System.out.println(n + " is Harshad number");
         else
-            System.out.println(n+ " is not a Harshad number");
+            System.out.println(n + " is not a Harshad number");
     }
 }
